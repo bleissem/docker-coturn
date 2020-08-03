f5d3fead9f427c9223bd6b27116f628adc2f56af
Alexander Bleissem <alexander@bleissem.com>
2020-08-03 22:30:37 +0200

    changes

--- stun/entrypoint.sh
+++ stun/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-exec /usr/bin/turnserver -n -v -f --no-cli --mobility -p 3478 --tls-listening-port=5349 --min-port 49152 --max-port 65535 --external-ip=$externalip -a --user $user:$password -r $realm --cert=/certs/cert.pem --pkey=/certs/privkey.pem
+while [ ! -f /certs/cert.pem ]
+do
+  echo 'wait for file cert.pem'
+  sleep 5
+done
+
+while [ ! -f /certs/privkey.pem ]
+do
+  echo 'wait for file privkey.pem'
+  sleep 5  
+done
+
+exec /usr/bin/turnserver -n -v -f --no-cli --stun-only --mobility -p 3478 --tls-listening-port=5349 --min-port 49152 --max-port 65535 --external-ip=$externalip -a --user $user:$password -r $realm --cert=/certs/cert.pem --pkey=/certs/privkey.pem
 
