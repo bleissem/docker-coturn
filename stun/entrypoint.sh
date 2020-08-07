4d01ff47e52a08266916ee419ee3fe3e71d37548
Alexander Bleissem <alexander@bleissem.com>
2020-08-07 19:04:32 +0200

    add whattostart

--- stun/entrypoint.sh
+++ stun/entrypoint.sh
@@ -12,5 +12,5 @@ do
   sleep 5  
 done
 
-exec /usr/bin/turnserver -n -v -f --no-cli --stun-only --mobility -p 3478 --tls-listening-port=5349 --min-port 49152 --max-port 65535 --external-ip=$externalip -a --user $user:$password -r $realm --cert=/certs/cert.pem --pkey=/certs/key.pem
+exec /usr/bin/turnserver -n -v -f --no-cli $whattostart --mobility -p 3478 --tls-listening-port=5349 --min-port 49152 --max-port 65535 --external-ip=$externalip -a --user $user:$password -r $realm --cert=/certs/cert.pem --pkey=/certs/key.pem
 
